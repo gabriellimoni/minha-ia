d71c67ae8e0a5366ae4ce9be4d17488e843d07db
Gabriel Limoni <gabrielblimoni@gmail.com>
2025-08-06 17:22:58 -0300

    add express

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,20 @@
-import ollama from "ollama";
+// import ollama from "ollama";
+import express, { json, type Request, type Response } from "express";
 
-const response = await ollama.chat({
-  model: "gemma3:1b",
-  messages: [{ role: "user", content: "Why is the sky blue?" }],
+// const response = await ollama.chat({
+//   model: "gemma3:1b",
+//   messages: [{ role: "user", content: "Why is the sky blue?" }],
+// });
+// console.log(response.message.content);
+
+const app = express();
+app.use(json());
+
+app.post("/generate-commit-message", (req: Request, res: Response) => {
+  res.status(200).send(req.body);
 });
-console.log(response.message.content);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Listening on", port);
+});
