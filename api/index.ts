af2b703e0710df3c76e96487141b7eb6a37f26b5
Gabriel Limoni <gabrielblimoni@gmail.com>
2025-08-06 17:16:00 -0300

    add ollama

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import ollama from "ollama";
+
+const response = await ollama.chat({
+  model: "gemma3:1b",
+  messages: [{ role: "user", content: "Why is the sky blue?" }],
+});
+console.log(response.message.content);
