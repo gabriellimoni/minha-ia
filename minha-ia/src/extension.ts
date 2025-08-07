bd8d12672d68b610c2deaa18d211a8ea62bf6c9e
Gabriel Limoni <gabrielblimoni@gmail.com>
2025-08-07 16:32:23 -0300

    getting diff from the repo

--- minha-ia/src/extension.ts
+++ minha-ia/src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { exec } from "child_process";
 import * as vscode from "vscode";
 
 // This method is called when your extension is activated
@@ -13,16 +14,48 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "minha-ia.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const text = "AE CARAIO 5";
-      vscode.window.showInformationMessage(text);
+    "minha-ia.generateMessage",
+    async () => {
+      const diff = await getGitDiffAsText();
+      console.log("diff", diff);
+
+      vscode.window.showInformationMessage(String(diff));
     }
   );
 
   context.subscriptions.push(disposable);
+}
+
+async function getGitDiffAsText() {
+  const gitExtension = vscode.extensions.getExtension("vscode.git");
+  if (!gitExtension) {
+    vscode.window.showErrorMessage("Git extension not found.");
+    return;
+  }
+
+  const git = gitExtension.exports.getAPI(1);
+  console.log("git", Object.keys(git));
+  if (git.repositories.length === 0) {
+    vscode.window.showInformationMessage(
+      "No Git repositories found in the workspace."
+    );
+    return;
+  }
+
+  const repository = git.repositories[0];
+  return getDiff(repository.rootUri.path);
+}
+
+async function getDiff(path: string): Promise<string> {
+  return new Promise((res) => {
+    exec(`git -C ${path} diff`, (error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      res(`Git Diff:\n${stdout}`);
+    });
+  });
 }
 
 // This method is called when your extension is deactivated
